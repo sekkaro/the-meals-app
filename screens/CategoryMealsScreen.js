5d0a9b88c3265c7dc6c7203ac518d65114cd3a2d
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-15 16:44:21 +0900

    setting dynamic navigation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = props => {
     const catId = props.navigation.getParam('categoryId');
 
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
-    
+
     return (
         <View style={styles.screen}>
             <Text>The Category Meal Screen!</Text>
@@ -17,9 +18,28 @@ const CategoryMealsScreen = props => {
                 });
             }}
             />
-            <Button />
+            <Button
+                title="Go Back"
+                onPress={() => {
+                    props.navigation.pop();
+                }}
+            />
         </View>
     );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
