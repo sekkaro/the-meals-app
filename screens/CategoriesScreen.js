f51280d927a539711865041755592c07e57995be
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-15 16:17:31 +0900

    outputting a grid of categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+const renderGridItem = (itemData) => {
+    return (
+        <View style={styles.gridItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    );
+};
 
 const CategoriesScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen!</Text>
-            <Button title="Go to Meals!" onPress={() => {
-                props.navigation.navigate({ routeName: 'CategoryMeals' });
-            }} />
-        </View>
+        <FlatList
+            keyExtractor={(item, index) => item.id}
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            numColumns={2}
+        />
     );
 };
 
@@ -17,6 +27,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
     }
 });
 
