5eb1d19951bbfd18ba50c9c22bf77decd93a721f
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-19 17:13:47 +0900

    redux & store setup

--- App.js
+++ App.js
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 let customFonts = {
   'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -30,7 +39,9 @@ export default class App extends React.Component {
   render() {
     if (this.state.fontsLoaded) {
       return (
-        <MealsNavigator />
+        <Provider store={store}>
+          <MealsNavigator />
+        </Provider>
       );
     } else {
       return (
