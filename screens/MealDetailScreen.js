e83b60ab2914d610e5d23b39d674c55fbf6449bf
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-19 15:54:21 +0900

    adding filters screen content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 200
     },
-    detail: {
+    details: {
         flexDirection: 'row',
         padding: 15,
         justifyContent: 'space-around'
