2d3be379698934d498efc2ea6241c7282e2abca3
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-20 03:20:20 +0900

    dispatching filter actions

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -22,7 +22,7 @@ const mealsReducer = (state = initialState, action) => {
                 return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
             }
         case SET_FILTERS:
-            const appliedFilters = actions.filters;
+            const appliedFilters = action.filters;
             const updatedFilteredMeals = state.meals.filter(meal => {
                 if (appliedFilters.glutenFree && !meal.isGlutenFree) {
                     return false;
